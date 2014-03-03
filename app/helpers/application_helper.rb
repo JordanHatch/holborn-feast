ca5f06d6b7052368e1a4c591913f21be9032f329
Jordan Hatch <jordan@jordanh.net>
2014-03-03 08:49:42 +0000

    Move signed in user bar to footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def user_with_image(user)
+    content_tag :div, { :class => "user-with-image" } do
+      style = "background-image: url('" + user.image_url + "')"
+      content_tag(:span, "", :class => "image", :style => style) + user.name
+    end
+  end
 end
