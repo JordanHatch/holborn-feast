2f4afad4cf1558de47c50dd19f816891f0703135
Jordan Hatch <jordan@jordanh.net>
2014-03-03 08:57:21 +0000

    Don't fall over when no image present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
   def user_with_image(user)
     content_tag :div, { :class => "user-with-image" } do
-      style = "background-image: url('" + user.image_url + "')"
-      content_tag(:span, "", :class => "image", :style => style) + user.name
+      user_image = ""
+      if user.image_url.present?
+        style = "background-image: url('" + user.image_url + "')"
+        user_image = content_tag(:span, "", :class => "image", :style => style)
+      end
+
+      user_image + user.name
     end
   end
 end
